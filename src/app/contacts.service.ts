f36b0ca845bc64eaa801f88c82626e7443c50923
christianarredondo <christianarredondo@utexas.edu>
2017-08-23 16:15:06 -0500

    begin refactoring search to service

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -1,9 +1,14 @@
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/merge';
 import { Injectable } from '@angular/core';
 import { CONTACT_DATA } from './data/contact-data';
 import { Observable } from 'rxjs/Observable';
 import { Contact } from './models/contact';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs/Subject';
 
 interface ContactResponse  { item  : Contact }
 interface ContactsResponse { items : Contact[] }
@@ -11,6 +16,7 @@ interface ContactsResponse { items : Contact[] }
 @Injectable()
 export class ContactsService {
   API_ENDPOINT = 'http://localhost:4201';
+  private terms$ = new  Subject<string>();
   
   constructor(
     private http: HttpClient,
@@ -43,7 +49,25 @@ export class ContactsService {
     return this.http.get<ContactsResponse>(url)
       .map(data => data.items);
   }
+
+  rawSearch (term: string) {
+    let url = `http://localhost:4201/api/search?text=${term}`;
+    return this.http.get<ContactsResponse>(url)
+      .map(data => data.items);
+  }
   
+  // rawSearch (term: string) {
+  //   let urlStatic = `http://localhost:4201/api/contacts`;
+  //   let urlDynamic = `http://localhost:4201/api/search?text=${term}`;
+  //   let first$ = this.getContacts();
+  //   let later$ = this.terms$
+  //     .debounceTime(200)
+  //     .distinctUntilChanged()
+  //     .switchMap(term => this.http.get<ContactsResponse>(urlDynamic)
+  //       .map(data => data.items)
+  //     )};
+  // }
+
 }
 
 
