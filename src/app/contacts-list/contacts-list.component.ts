8e62b04ef8cdd9c245ba2a1c3f32e3dd16e7be49
christianarredondo <christianarredondo@utexas.edu>
2017-08-23 11:25:48 -0500

    add async pipe to contacts list

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { Contact } from '../models/contact';
 import { ContactsService } from '../contacts.service';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 @Component({
@@ -9,14 +10,13 @@ import { ContactsService } from '../contacts.service';
   styleUrls: ['./contacts-list.component.css']
 })
 export class ContactsListComponent implements OnInit {
-  contacts: Contact[];
+  contacts$: Observable<Array<Contact>>;
   
   constructor(contactsService: ContactsService) {
-    contactsService.getContacts()
-      .subscribe(contacts => {
-          this.contacts = contacts;
-      })};
-  
+    this.contacts$ = contactsService.getContacts();
+    contactsService.getContacts();
+  }
+
   ngOnInit() {}
 
 }
